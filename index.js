f234843b8d7a648870536ea51666d6df2a3fd662
Peter Kha <khapeterk@gmail.com>
2021-12-04 09:55:04 -0500

    fix wrong var

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const ws = require('ws');
 const http = require('http');
 
 const app = express();
-const server = http.createServer(app);
+app.use((req, res) => res.sendFile('/index.html', { root: __dirname }))
+
 const port = process.env.PORT || 3000
-server.use((req, res) => res.sendFile('/index.html', { root: __dirname }))
-server.listen(port, () => {
-	console.log(`server started on part ${port}`)
+app.listen(port, () => {
+  console.log(`server started on part ${port}`)
 })
 
+const server = http.createServer(app);
 const wss = new ws.Server({ server })
 
 wss.on('connection', (ws) => {
